ecfa719427ca44ac8e31055b8f01c5ebd79869e7
ramjiresearch <ramjiresearchdl@gmail.com>
2024-07-31 15:11:12 +0530

    Update app.py made ramji

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from forms import ToDoForm
-
+#edit a
+#ramji
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
